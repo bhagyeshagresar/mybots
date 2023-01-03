a8e88a7913211093ce00f85846a29093b66f0ad5
bhagyeshagresar <bhagyeshagresar@gmail.com>
2023-01-03 00:34:52 -0600

    second block sits on top of first

--- simulate.py
+++ simulate.py
@@ -11,7 +11,7 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 p.setGravity(0, 0, -9.8)
 planplaneId = p.loadURDF("plane.urdf")
-p.loadSDF("box.sdf")
+p.loadSDF("boxes.sdf")
 
 for i in range(1000):
     p.stepSimulation()
